04da2f7ff51377f32f6af1ea52b1489150a87197
贺俊浩 <nougat_hx@qq.com>
2024-01-10 19:51:39 +0800

    fix: auth - get

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,12 @@
 async function authSlat(ctx, next) {
   const { slat } = ctx.request.body;
+  console.log();
+  if (ctx.request.method === "GET") {
+    await next();
+    return;
+  }
   if ((slat || "").toString() !== process.env.SLAT.toString()) {
+    ctx.status = 404;
     return (ctx.body = {
       success: false,
       message: "验证失败",
